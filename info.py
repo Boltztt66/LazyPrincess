e9ee987a0795cea5261be44f366ccf077bb95caf
Boltztt66 <136881441+Boltztt66@users.noreply.github.com>
2023-09-16 07:47:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://te.legra.ph/file/f0a95bd9a420e6e1296fb.jpg')
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/f0a95bd9a420e6e1296fb.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
